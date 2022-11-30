1a43db348ed40ef6f697eb14d123ef2d56f2514b
Sam AL-Arbid <samaa2@kth.se>
2022-12-01 00:52:28 +0100

    fix render uncertainty for deposit view override

--- invenio_theme_kth/config.py
+++ invenio_theme_kth/config.py
@@ -1,3 +1,7 @@
 # Invenio-Theme
 # =============
 # See https://invenio-theme.readthedocs.io/en/latest/configuration.html
+
+
+APP_THEME = ["semantic-ui"]
+"""The application theme to use."""
--- invenio_theme_kth/views.py
+++ invenio_theme_kth/views.py
@@ -1,19 +1,50 @@
 """Additional views."""
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import login_required
-from invenio_app_rdm.records_ui.views.deposits import deposit_create, deposit_edit
+from invenio_app_rdm.records_ui.views.decorators import (
+    pass_draft,
+    pass_draft_community,
+    pass_draft_files,
+)
+from invenio_app_rdm.records_ui.views.deposits import (
+    get_form_config,
+    get_search_url,
+    new_record,
+)
+from invenio_rdm_records.resources.serializers import UIJSONSerializer
 
 
 @login_required
-def guarded_deposit_create(*args, **kwargs):
-    """Guard the creation page for records, based on permissions."""
-    return deposit_create(*args, **kwargs)
+@pass_draft_community
+def deposit_create_override(community=None):
+    """Create a new deposit."""
+    return render_template(
+        "invenio_app_rdm/records/deposit_override.html",
+        forms_config=get_form_config(createUrl="/api/records"),
+        searchbar_config=dict(searchUrl=get_search_url()),
+        record=new_record(),
+        files=dict(default_preview=None, entries=[], links={}),
+        preselectedCommunity=community,
+    )
 
 
 @login_required
-def guarded_deposit_edit(*args, **kwargs):
-    """Guard the edit page for records, based on permissions."""
-    return deposit_edit(*args, **kwargs)
+@pass_draft(expand=True)
+@pass_draft_files
+def deposit_edit_override(pid_value, draft=None, draft_files=None):
+    """Edit an existing deposit."""
+    files_dict = None if draft_files is None else draft_files.to_dict()
+    ui_serializer = UIJSONSerializer()
+    record = ui_serializer.dump_obj(draft.to_dict())
+
+    return render_template(
+        "invenio_app_rdm/records/deposit_override.html",
+        forms_config=get_form_config(apiUrl=f"/api/records/{pid_value}/draft"),
+        record=record,
+        files=files_dict,
+        searchbar_config=dict(searchUrl=get_search_url()),
+        permissions=draft.has_permissions_to(["new_version", "delete_draft"]),
+    )
 
 
 def create_blueprint(app):
@@ -32,9 +63,9 @@ def create_blueprint(app):
         """Override the existing view functions with more access control."""
         app.view_functions[
             "invenio_app_rdm_records.deposit_edit"
-        ] = guarded_deposit_edit
+        ] = deposit_edit_override
         app.view_functions[
             "invenio_app_rdm_records.deposit_create"
-        ] = guarded_deposit_create
+        ] = deposit_create_override
 
     return blueprint
