97925e96d7f947ddea37050831e5b44039777d05
Sam AL-Arbid <samaa2@kth.se>
2022-12-01 01:00:42 +0100

    fix test double blueprint registration

--- invenio_theme_kth/ext.py
+++ invenio_theme_kth/ext.py
@@ -15,33 +15,7 @@ class InvenioThemeKTH:
     def init_app(self, app):
         """Flask application initialization."""
         self.init_config(app)
-        self.init_blueprint(app)
         app.extensions["invenio-theme-kth"] = self
-
-    def init_blueprint(self, app):
-        """Initialize blueprint."""
-        app.register_blueprint(self.blueprint)
-        # since invenio-app-rdm currently (october 2022) doesn't offer an easy way of
-        # overriding the provided jinja templates, we have to perform a workaround:
-        # the first blueprint that has a definition for a template (per name) gets
-        # selected by 'flask.render_template()' - thus, we just have to make sure that
-        # our blueprint is inserted before the invenio_app_rdm blueprints
-        #
-        # this has to be done after everything (including *all* blueprints)
-        # has been initialized, and is only really relevant for the UI
-        # thus, we use `app.before_first_request` rather than a startup blueprint
-        #
-        # NOTE: the below code requires Flask 2.0.2+ and Python 3.7+
-        #       for insertion-ordered dictionaries
-        @app.before_first_request
-        def register_kth_bp_first():
-            bps = {
-                name: bp
-                for name, bp in app.blueprints.items()
-                if name != "invenio_theme_kth"
-            }
-
-            app.blueprints = {"invenio_theme_kth": self.blueprint, **bps}
 
     def init_config(self, app):
         """Initialize configuration."""
