4a65b5f6d063ce39c87cc4c7097b8a83a031bd6f
Sam AL-Arbid <samaa2@kth.se>
2022-12-03 03:22:03 +0100

    i18n: setup swedish translation

---
+++ invenio_theme_kth/assets/semantic-ui/translations/invenio_app_rdm/messages/index.js
@@ -0,0 +1,7 @@
+import TRANSLATE_EN from "./en/translations.json";
+import TRANSLATE_SV from "./sv/translations.json";
+
+export const translations = {
+  en: { translation: TRANSLATE_EN },
+  sv: { translation: TRANSLATE_SV },
+};
